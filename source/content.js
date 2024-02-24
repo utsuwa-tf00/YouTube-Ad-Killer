7c7dc542fd1e87d0669bbe49d1b55dfd28347fc2
DESKTOP-QOR0603\masaa <masaaki.nakata@gmail.com>
2024-02-25 02:03:19 +0900

    ver. 1.1.8 YouTubeによるスキップボタン自動クリック機能の摘発への対応 プログラムの最適化

--- source/content.js
+++ source/content.js
@@ -3,12 +3,16 @@ let oldMovieKillerEnabled = false;
 document.addEventListener("keydown", function (event) {
   if (event.key === "Delete") {
     oldMovieKillerEnabled = !oldMovieKillerEnabled;
+    const text = `oldMovieKiller は ${
+      oldMovieKillerEnabled ? "有効です" : "無効です"
+    }`;
+    console.log(text);
   }
 });
 
 // YouTubeページ上でのDOM変更を監視する
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
     const url = window.location.href;
     if (url === "https://www.youtube.com/") {
       youTubeHomeAdKiller();
@@ -18,49 +22,99 @@ const observer = new MutationObserver((mutations) => {
         oldMovieKiller();
       }
     } else if (url.includes("https://www.youtube.com/watch?")) {
-      youTubeVideoPageAdKiller(mutation);
+      youTubePlayerAdKiller(mutation);
     }
   });
 });
 
 // YouTube動画ページの変更を処理する
-function youTubeVideoPageAdKiller(mutation) {
+function youTubePlayerAdKiller(mutation) {
   if (mutation.addedNodes.length || mutation.removedNodes.length) {
-    attemptToSkipAd();
+    skipPlayerAd();
   }
   removePlayerAds();
+  /*
   if (oldMovieKillerEnabled) {
     oldMovieKiller();
   }
+  */
   removePromoPopups();
 }
 
 // 広告をスキップする試み
-function attemptToSkipAd() {
+function removeAdModule() {
+  const interstitialCard = document.querySelectorAll(
+    "div.ytp-ad-action-interstitial-card"
+  );
+
+  if (interstitialCard > 0) {
+    console.log("youTubePlayerAdKiller : 広告要素削除");
+    interstitialCard.forEach(function (card) {
+      card.remove();
+    });
+  }
+
+  /*
   const skipButton = document.querySelector(
     ".ytp-ad-skip-button-modern.ytp-button"
   );
-  if (skipButton) {
-    skipButton.click();
-  } else {
-    muteAndHideAd();
-  }
+  // スキップボタン内の特定のdivを検索
+  const skipButtonText = skipButton
+    ? skipButton.querySelector(
+        "div.ytp-ad-text.ytp-ad-skip-button-text-centered.ytp-ad-skip-button-text"
+      )
+    : null;
+  // スキップ可能時のボタンテキスト
+  const texts = ["スキップ", "skip"];
+  
+  // スキップボタンとテキスト内容の両方が存在し、テキスト内容が"スキップ"であるか確認
+  texts.forEach(function (text) {
+    if (skipButton && skipButtonText.textContent === text) {
+      
+      if (adModule) {
+        adModule.remove();
+      }
+    }
+  });
+  */
 }
 
 // 広告をミュートして隠す
-function muteAndHideAd() {
-  const adModule = document.querySelector(".video-ads.ytp-ad-module");
+function skipPlayerAd() {
+  //const adModule = document.querySelector(".video-ads.ytp-ad-module");
   const video = document.querySelector("video");
-  if (adModule && isElementVisible(adModule)) {
-    video.style.opacity = 0;
-    video.volume = 0;
-    if (video.currentTime <= video.duration) {
-      video.currentTime = video.duration + 1;
+
+  const videoContainer = document.querySelector(
+    "div#container.style-scope.ytd-player"
+  );
+
+  if (!video) {
+    return;
+  }
+
+  if (videoContainer.querySelector(".ad-showing")) {
+    if (video.currentTime < video.duration) {
+      console.log("youTubePlayerAdKiller : 広告スキップ");
+      video.style.filter = "brightness(0%)";
+      video.volume = 0;
+      video.currentTime = video.duration;
     }
   } else {
-    if (video.style.opacity != 1) {
-      video.style.opacity = 1;
+    if (video.style.filter == "brightness(0%)") {
+      video.style.filter = "none";
     }
+  }
+
+  const adImage = document.querySelectorAll("img.ytp-ad-image");
+  if (adImage.length > 0) {
+    adImage.forEach(function (img) {
+      img.remove("youTubePlayerAdKiller : 広告画像を削除");
+    });
+    console.log();
+  }
+
+  if (video.currentTime >= video.duration) {
+    removeAdModule();
   }
 }
 
@@ -69,9 +123,14 @@ function removePlayerAds() {
   const playerAds = document.querySelectorAll(
     "div#player-ads.style-scope.ytd-watch-flexy, ytd-ad-slot-renderer.style-scope.ytd-watch-next-secondary-results-renderer"
   );
-  playerAds.forEach((element) => {
-    element.remove();
-  });
+  if (playerAds.length > 0) {
+    console.log(
+      `youTubePlayerAdKiller : バナー広告 ${playerAds.length} 個 削除`
+    );
+    playerAds.forEach(function (element) {
+      element.remove();
+    });
+  }
 }
 
 // プロモーションポップアップを削除する
@@ -79,9 +138,14 @@ function removePromoPopups() {
   const ytMealbarPromoRenderer = document.querySelectorAll(
     "div#main.style-scope.yt-mealbar-promo-renderer"
   );
-  ytMealbarPromoRenderer.forEach((element) => {
-    element.remove();
-  });
+  if (ytMealbarPromoRenderer.length > 0) {
+    console.log(
+      `youTubePlayerAdKiller : プロモーションポップアップ ${ytMealbarPromoRenderer.length} 個 削除`
+    );
+    ytMealbarPromoRenderer.forEach(function (element) {
+      element.remove();
+    });
+  }
 }
 
 // 要素が表示されているかどうかを判断する
@@ -92,40 +156,64 @@ function isElementVisible(element) {
 
 // YouTubeホームページ上の広告を削除する
 function youTubeHomeAdKiller() {
-  const possibleAdElements = document.querySelectorAll(
-    "ytd-rich-item-renderer.style-scope, ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer, div#masthead-ad.style-scope.ytd-rich-grid-renderer"
+  // 最初に広告要素を取得
+  const adElements = document.querySelectorAll(
+    "ytd-in-feed-ad-layout-renderer.style-scope.ytd-ad-slot-renderer, ytd-ad-slot-renderer.style-scope.ytd-rich-item-renderer"
   );
 
-  possibleAdElements.forEach((element) => {
-    // ytd-rich-item-renderer内の広告要素を検出し、削除
-    if (element.matches("ytd-rich-item-renderer.style-scope")) {
-      const adElements = element.querySelectorAll(
-        "ytd-in-feed-ad-layout-renderer.style-scope.ytd-ad-slot-renderer, ytd-ad-slot-renderer.style-scope.ytd-rich-item-renderer"
-      );
-      if (adElements.length > 0) {
-        element.remove();
-      }
-    }
-    // div#masthead-adを直接削除
-    if (element.matches("div#masthead-ad.style-scope.ytd-rich-grid-renderer")) {
-      element.remove();
-    }
+  if (adElements.length > 0) {
+    console.log(
+      `youTubeHomeAdKiller : 広告サムネイル ${adElements.length} 個 削除`
+    );
 
-    if (
-      element.matches(
-        "ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer"
-      )
-    ) {
-      element.remove();
-    }
-  });
+    // 各広告要素に対して処理を実行
+    adElements.forEach(function (adElement) {
+      // 親要素を辿ってytd-rich-item-rendererを探す
+      let parentElement = adElement.parentElement;
+      while (
+        parentElement &&
+        !parentElement.matches("ytd-rich-item-renderer.style-scope")
+      ) {
+        parentElement = parentElement.parentElement;
+      }
+
+      // 見つかったら削除
+      if (
+        parentElement &&
+        parentElement.matches("ytd-rich-item-renderer.style-scope")
+      ) {
+        parentElement.remove();
+      }
+    });
+  }
+
+  const mastheadAd = document.querySelector(
+    "div#masthead-ad.style-scope.ytd-rich-grid-renderer"
+  );
+  if (mastheadAd) {
+    console.log("youTubeHomeAdKiller : マストヘッド広告削除");
+    mastheadAd.remove();
+  }
+
+  // ytd-rich-section-rendererの広告を直接削除
+  const richSectionAds = document.querySelectorAll(
+    "ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer"
+  );
+  if (richSectionAds.length > 0) {
+    console.log(
+      `youTubeHomeAdKiller : 大広告を ${richSectionAds.length} 個 削除`
+    );
+    richSectionAds.forEach(function (ad) {
+      ad.remove();
+    });
+  }
 }
 
 // ショート動画を削除する
 function shortsKiller() {
   const short = document.querySelector("ytd-rich-shelf-renderer");
   if (short) {
-    //console.log("YouTubeホーム : ショート動画を削除しました");
+    console.log("shortsKiller : ショート動画を削除");
     short.remove();
   }
 
@@ -133,7 +221,7 @@ function shortsKiller() {
     'a#endpoint[title="ショート"], a#endpoint[title="Shorts"]'
   );
   if (homeShortTab) {
-    //console.log("YouTubeホーム : ホーム画面のショートのタブを削除しました");
+    console.log("shortsKiller : ショート動画タブを削除");
     homeShortTab.parentElement.remove();
   }
 }
@@ -152,24 +240,31 @@ function oldMovieKiller() {
     "years ago",
   ];
 
-  const possibleOldMovieElements = document.querySelectorAll(
-    "ytd-rich-item-renderer.style-scope"
+  // 直接条件にマッチするspan要素を取得
+  var spanElements = document.querySelectorAll(
+    "span.inline-metadata-item.style-scope.ytd-video-meta-block"
   );
-
-  possibleOldMovieElements.forEach((element) => {
-    const spanElements = element.querySelectorAll(
-      "span.inline-metadata-item.style-scope.ytd-video-meta-block"
-    );
-
-    spanElements.forEach((spanElement) => {
-      const spanTextContent = spanElement.textContent;
-      spansText.forEach((text) => {
-        if (spanTextContent.includes(text)) {
-          element.remove();
-        }
+  if (spanElements.length > 0) {
+    console.log(`oldMovieKiller : 古い動画を ${spanElements.length} 個 削除`);
+    spanElements.forEach(function (spanElement) {
+      var spanTextContent = spanElement.textContent;
+      var isOldVideo = spansText.some(function (text) {
+        return spanTextContent.includes(text);
       });
+
+      if (isOldVideo) {
+        // 親要素を辿ってytd-rich-item-rendererを探す
+        var parentElement = spanElement.closest(
+          "ytd-rich-item-renderer.style-scope"
+        );
+
+        // 見つかったら削除
+        if (parentElement) {
+          parentElement.remove();
+        }
+      }
     });
-  });
+  }
 }
 
 // MutationObserverを設定して、DOM変更を監視する
